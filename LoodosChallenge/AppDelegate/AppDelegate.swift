267f03a6da50abd3b0a38a413b858486a1d0cd37
OktayKurt <kurt21380@gmail.com>
2024-11-08 10:47:16 +0300

    Dev first commit.

---
+++ LoodosChallenge/AppDelegate/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  LoodosChallenge
+//
+//  Created by Oktay's Macbook on 8.11.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
