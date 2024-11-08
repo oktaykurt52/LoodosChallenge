ff4c0d99f5cbce5857e9abfe43634d4355aff941
OktayKurt <kurt21380@gmail.com>
2024-11-08 10:33:11 +0300

    Initial Commit

---
+++ LoodosChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoodosChallenge
+//
+//  Created by Oktay's Macbook on 8.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
